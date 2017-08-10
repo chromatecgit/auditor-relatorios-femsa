b9411640cb1fa610481e280918ac7ffaf00cc660
csportore <cesar.sportore@shelfpix.com.br>
2017-08-10 14:01:39 -0300

    New Auditors and upgrades
    - AuditorValoresIguais
    - Upgraded VelhoNovoExtractor

---
+++ src/main/java/main/AuditorValoresIguais.java
@@ -0,0 +1,40 @@
+package main;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import extractors.ConsolidadoPrecoExtractor;
+import extractors.ValoresIguaisExtractor;
+import utils.MyFileVisitor;
+
+public class AuditorValoresIguais {
+	public static void main(String[] args) {
+		System.out.println("AuditorConsolidadoPreco");
+		try {
+			String glob = "glob:**/*.{xlsx}";
+			
+			Path pathRef = generateReferenceFilePath();
+			ValoresIguaisExtractor extractor = new ValoresIguaisExtractor();
+			MyFileVisitor fv1 = new MyFileVisitor(pathRef, glob);
+			
+			extractor.getResults(fv1.getPaths());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private static Path generateReferenceFilePath() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(
+				System.getProperty("user.dir"))
+				.append(File.separator)
+				.append("src")
+				.append(File.separator)
+				.append("consolidado-preco")
+				.append(File.separator)
+				.append("ref");
+		return Paths.get(sb.toString());
+	}
+}
